9e8011e6c3eec7f60036a96fe58bbf9bf2bf652d
thebladehit <boqdanyarmolka@gmail.com>
2025-05-15 22:24:12 +0300

    Add ConfigModule and validation for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
